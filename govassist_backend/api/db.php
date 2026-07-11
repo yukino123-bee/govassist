1c7b951fc5a8a2f655c6f39ff51a807ca9a2fae9
yukino123-bee <yukino123-bee@users.noreply.github.com>
2026-07-11 19:34:54 +0100

    Switch to direct deployment workflow and remove flattening pipeline

--- govassist_backend/api/db.php
+++ govassist_backend/api/db.php
@@ -1,10 +1,10 @@
 <?php
 // backend/api/db.php
 
-$host = 'localhost';
-$dbname = 'govassist_db';
-$username = 'root';
-$password = '';
+$host = 'fdb1032.awardspace.net';
+$dbname = '4715301_govassist';
+$username = '4715301_govassist';
+$password = 'manok123#';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
